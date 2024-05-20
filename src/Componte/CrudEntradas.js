591f9f19986f300b2dcdef00aaeec61bf8d6c669
andriusdesarolladorweb <112970410+Andriusdesarolladorweb@users.noreply.github.com>
2024-05-20 19:55:39 +0000

    Actulizaciones tabla crud

--- src/Componte/CrudEntradas.js
+++ src/Componte/CrudEntradas.js
@@ -2,6 +2,15 @@ import "../css/CrudEntradas.css";
 import React, { useState } from "react";
 
 export default function CrudEntradas() {
+  const mostrar = [
+    { id: 1, monto: 15000, fecha: 2 / 5 / 2024, concepto: "Mami" },
+    { id: 1, monto: 15000, fecha: 2 / 5 / 2024, concepto: "Mami" },
+    { id: 1, monto: 15000, fecha: 2 / 5 / 2024, concepto: "Mami" },
+    { id: 1, monto: 15000, fecha: 2 / 5 / 2024, concepto: "Mami" },
+    { id: 1, monto: 15000, fecha: 2 / 5 / 2024, concepto: "Mami" },
+  ];
+  const [data, setData] = useState(mostrar);
+
   return (
     <div>
       <h2 className="text-center p-5"> Registro De Entradas fijas </h2>
@@ -9,14 +18,14 @@ export default function CrudEntradas() {
         <form className=" bg-secondary p-5 rounded-5 text-white">
           <br />
           <div className="mb-3 ">
-            <label for="Usuario" className="form-label">
-              Usuario
+            <label for="Monto" className="form-label">
+              Monto
             </label>
             <input
               type="text"
               className="form-control"
-              id="Usuario"
-              name="Usuario"
+              id="Monto"
+              name="Monto"
             />
           </div>
           <div className="mb-3 mt-3">
@@ -48,16 +57,28 @@ export default function CrudEntradas() {
             Agregar
           </button>
         </form>
-        <table>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Nombre</th>
-              <th>Minutos (por día)</th>
-              <th>Acciones</th>
-            </tr>
-          </thead>
-        </table>
+        <div className=" p-5">
+          <table className="table table-bordered ">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Monto</th>
+                <th>Fecha</th>
+                <th>Concepto</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((elemento) => (
+                <tr>
+                  <td>{elemento.id}</td>
+                  <td>{elemento.monto}</td>
+                  <td>{elemento.fecha}</td>
+                  <td>{elemento.concepto}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
